a03f965fd4cf5bd51eb9d9ea7f8d2d5185723e45
Elias Funes <eliasfunesriveros@gmail.com>
2022-02-20 20:56:16 -0300

    Initial commit

---
+++ src/main/java/com/example/messagingstompwebsocket/models/Greeting.java
@@ -0,0 +1,17 @@
+package com.example.messagingstompwebsocket.models;
+
+public class Greeting {
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
